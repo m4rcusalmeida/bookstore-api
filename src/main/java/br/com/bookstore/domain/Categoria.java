db82ae101646a1a6f6308d1692031b59662e2e43
m4rcusalmeida <m4rcus.almeida@gmail.com>
2022-03-15 17:56:29 -0300

    Adicionado relacionamentos nas classes de domain

--- src/main/java/br/com/bookstore/domain/Categoria.java
+++ src/main/java/br/com/bookstore/domain/Categoria.java
@@ -1,18 +1,30 @@
 package br.com.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-public class Categoria {
-
+@Entity
+public class Categoria implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String descricao;
 
-	@OneToMany
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<Livro>();
 
 	public Categoria() {
--- src/main/java/br/com/bookstore/domain/Livro.java
+++ src/main/java/br/com/bookstore/domain/Livro.java
@@ -1,17 +1,31 @@
 package br.com.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 
 	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
